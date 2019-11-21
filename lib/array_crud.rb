af83a1887599a2e21e2fda2f40a088638fc906f8
Erik Kasperson <chefkasperson@gmail.com>
2019-11-21 01:08:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,26 +23,26 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  remove_element_from_start_of_array.shift
+  array.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_element_from_index[1]
+  array[1]
   
 end
 
 def retrieve_first_element_from_array(array)
-  retrieve_first_element_from_array[0]
+  array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
-  retrieve_last_element_from_array[-1]
+  array[-1]
   
 end
 
 def update_element_from_index(array, index_number, element)
-  update_element_from_index[1] = "aipim"
+  array[1] = "aipim"
 
 end
