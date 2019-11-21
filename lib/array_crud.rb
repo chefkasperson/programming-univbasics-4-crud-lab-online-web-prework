11cbb47f6bac160377c30e1a71cbafa62f71b817
Erik Kasperson <chefkasperson@gmail.com>
2019-11-21 01:05:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,11 +14,13 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array.unshift("banana")
+   array = ["guarana", "acerola", "pitanga", "castanha"]
+   array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  remove_element_from_end_of_array.pop
+   array = ["guarana", "acerola", "pitanga", "castanha"]
+   array.pop
   
 end
 
